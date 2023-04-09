c7f81dfe76a36d8f7cb145188b106cab19da9a51
Ajay kumar <114280879+sipthy@users.noreply.github.com>
2023-04-09 12:26:41 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,87 @@
+{
+  "name": "ajay kumar",
+  "email": "ajarts1996@gmail.com",
+  "phone": "+91 73587 91692",
+  "address": "1/1017 4th cross street thiruvengadam nagar vadakkupattu medavakkam koot road",
+  "summary": "Experienced software engineer with expertise in front-end development and user experience design.",
+  "skills": [
+
+    {
+      "name": "Adobe Illustrator",
+      "level": "Advanced"
+    }
+    {
+      "name": "Adobe Photoshop",
+      "level": "Advanced"
+    }
+      {
+      "name": "Adobe premiore pro",
+      "level": "Advanced"
+    }
+     {
+      "name": "Adobe XD",
+      "level": "Advanced"
+    }
+     {
+      "name": "Wordpress",
+      "level": "Advanced"
+    }
+    {
+      "name": "HTML",
+      "level": "Advanced"
+    },
+    {
+      "name": "CSS",
+      "level": "Advanced"
+    },
+    {
+      "name": "JavaScript",
+      "level": "Expert"
+    },
+    {
+      "name": "React",
+      "level": "Expert"
+    },
+    {
+      "name": "Node.js",
+      "level": "Advanced"
+    }
+  ],
+  "experience": [
+    {
+      "company": "Sri mookambikai infosolution",
+      "position": "Graphic designer",
+      "startDate": "2023-04-04",
+      "endDate": "2023-12-10",
+      "description": "Worked on a variety of projects using Adobe Creative suite, HTML, CSS and Wordpress."
+    },
+    {
+      "company": "product video.in",
+      "position": "Senior Graphic designer",
+      "startDate": "2022-01-28",
+      "endDate": "2023-02-25",
+      "description": "Worked on a variety of projects using Adobe Creative suite, and Wordpress."
+    }
+    {
+      "company": "Nittany creative service",
+      "position": "Artist",
+      "startDate": "2018-05-25",
+      "endDate": "2021-12-15",
+      "description": "Worked on a variety of projects using Adobe Creative suite."
+    }
+  ],
+  "education": [
+    {
+      "institution": "IMAGE Creative Education",
+      "degree": "Diploma in Game Technology",
+      "startDate": "2018-02-01",
+      "endDate": "2021-12-01"
+    }
+    {
+      "institution": "PSB Polytechnic College",
+      "degree": "Diploma in Mechanical Engineereing",
+      "startDate": "2011-03-01",
+      "endDate": "2014-04-01"
+    }
+  ]
+}
